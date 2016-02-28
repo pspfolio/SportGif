96a7e144c3b4d0be2e9265dde2b277af948900f3
PeppeS <pspfolio@gmail.com>
2016-02-28 13:50:07 +0200

    Setting up gif schema for mongodb

---
+++ SportGifs/app/models/GifModel.js
@@ -0,0 +1,20 @@
+﻿var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var GifSchema = Schema({
+	title: {type: String, required: true},
+	url: {type: String, required: true},
+	permalink: {type: String, required: true},
+	subreddit: { type: String, required: true },
+	created_at: {type: Date, default: Date.now}
+});
+
+// set created_at before saving it to db
+GifSchema.pre('save', function (next) {
+	this.created_at = new Date();
+	next();
+});
+
+GifSchema.index({ title : 1 });
+
+module.exports = mongoose.model('gif', GifSchema);
