804f903f70d2c729c1ebe13e37f6750102e9e834
PeppeS <pspfolio@gmail.com>
2016-02-28 12:59:10 +0200

    Project start

---
+++ SportGifs/server.js
@@ -0,0 +1,9 @@
+﻿var express = require('express');
+var port = process.env.PORT || 3000;
+var routes = require('./routes');
+var user = require('./routes/user');
+
+var app = express();
+app.listen(port);
+
+console.log('App listening on port ' + port);
