c2f2aaf924f6b2e2623f7382ce5076a3527073a0
PeppeS <pspfolio@gmail.com>
2016-02-28 13:28:23 +0200

    Npm mongoose and bodyparser

--- SportGifs/server.js
+++ SportGifs/server.js
@@ -1,9 +1,27 @@
 ﻿var express = require('express');
 var port = process.env.PORT || 3000;
+var mongoos = require('mongoose');
+var bodyParser = require('body-parser');
 var routes = require('./routes');
 var user = require('./routes/user');
-
 var app = express();
-app.listen(port);
 
+/*
+ * EXPRESS.JS CONFIGURE
+ */
+
+// connection to mongodb
+mongoose.connect('mongodb://localhost:27017');
+app.use(bodyParser.json()); 
+
+/*
+ * ROUTES 
+ */
+
+
+/*
+ * LISTEN 
+ */
+
+app.listen(port);
 console.log('App listening on port ' + port);
