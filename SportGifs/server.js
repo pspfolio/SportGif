baf77a7546f3e258fd484e4bc11229c0e872320c
PeppeS <pspfolio@gmail.com>
2016-03-01 16:46:25 +0200

    Removing UI items from backend project

--- SportGifs/server.js
+++ SportGifs/server.js
@@ -12,6 +12,12 @@ var app = express();
 mongoose.connect('mongodb://localhost:27017');
 app.use(bodyParser.json()); 
 
+app.use(function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Header', 'Origin, X-Requested-With, Content-Type, Accept');
+	next();
+});
+
 /*
  * ROUTES 
  */
