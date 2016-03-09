f7f64d8033b46020624371ad1d989c2beb77dee8
PeppeS <pspfolio@gmail.com>
2016-03-09 16:43:26 +0200

    Improving code readability

--- SportGifs/app/cron/cronTimer.js
+++ SportGifs/app/cron/cronTimer.js
@@ -1,95 +1,62 @@
 ﻿var mongoose = require('mongoose');
 var GifModel = require('../models/GifModel');
 var CronJob = require('cron').CronJob;
-var getData = require('../services/getData');
+var httpHelper = require('../services/httpHelper');
 var async = require('async');
 
 var cronJob = function () {
 	/*new CronJob('* * * * * *', function () {
 		console.log('You will see this message every second');
 	}, null, true, 'America/Los_Angeles');*/
-
-	var subreddits = ['nba'];
-	for (var i = 0; i < subreddits.length; i++) {
-		console.log(getData);
-		var t = getData(subreddits[i], initData);
+	var options = {
+		host: 'www.reddit.com',
+		path: '/r/nba/.json?limit=100'
 	}
 	
-	var gifModel = {
-		title: '',
-		url: '',
-		permalink: '',
-		subreddit: ''
-	};
-	
+	var subreddits = ['nba'];
+	for (var i = 0; i < subreddits.length; i++) {
+		httpHelper.getData(options, initData);
+	}
+
 	function initData(redditPosts) {
-		console.log('whuup', redditPosts);
+		// find streamable gifs from posts
 		var streamable = redditPosts.data.children.filter(function (item) {
 			return item.data.url.indexOf('streamable') > -1
 		});
 		
+		// make mongoose schema GifModels
 		var gifs = streamable.map(function (item) {
-			return createGif(item);
+			return new GifModel(item.data);
 		});
 		
-
-		getListOfGifsToAddDb(gifs, addDataToDb)
-	}
-	
-	function createGif(item) {
-		var gif = Object.create(gifModel);
-		gif.title = item.data.title,
-		gif.url = item.data.url,
-		gif.permalink = item.data.permalink,
-		gif.subreddit = item.data.subreddit
-
-		return gif;
-	}
-	
-	function getDataFromReddit(channel, callback) {
-
-
-		http.get(options, function (resp) {
-			var body = '';
-			resp.on('data', function (chunk) {
-				body += chunk;
-			}).on('end', function () {
-				var parsed = JSON.parse(body);
-				callback(parsed);
-			}).on('error', function (e) {
-				console.log('error', e);
-			});
+		saveToDb(gifs, function (err) {
+			if (err) console.log(err + ' saving failed');
+			console.log('Done!');
 		});
-	}
+	};
 	
-	function getListOfGifsToAddDb(data, cb) {
-		var gifCollection = [];
-		console.log(data.length);
-		async.each(data, function (item, callback) {
-			var gif = new GifModel(item);
+	function saveToDb(data, cb) {
+		console.log('saving gifs to database');
+		async.each(data, function (gif, callback) {
 			var query = GifModel.find({ url : gif.url });
 			query.exec(function (err, item) {
 				if (item.length > 0) {
 					callback();
 				} else {
-					gifCollection.push(gif);
-					callback();
+					// if the gif didn't find from db. Save it to the db.
+					gif.save(function (err) {
+						if (err) {
+							console.log(err + ' error saving the data to database');
+						} else {
+							console.log(gif.title + 'saved to the db');
+						}
+						callback();
+					});
 				}
 			});
 		}, function (err) {
-			console.log('done');
-			console.log(gifCollection.length);
-			cb(gifCollection)
-		});
-	}
-
-	function addDataToDb(gifs) {
-		async.each(gifs, function (gif, callback) {
-			gif.save(function (err) {
-				if (err) console.log('error saving the data to database');
-
-				console.log(gif.Title + ' saved to the db');
-			});
+			if (err) cb(err);
+			cb(null);
 		});
 	}
 }
--- SportGifs/app/services/httpHelper.js
+++ SportGifs/app/services/httpHelper.js
@@ -1,17 +1,14 @@
 ﻿var http = require('http');
 
-   function getData(options, cb) {
-	console.log('httpgetdata')
-	return http.get({
-		host: 'www.reddit.com',
-		path: '/r/nba/.json?limit=100'
-	}, function (resp) {
+var httpHelpers = function () { };
+
+httpHelpers.prototype.getData = function (options, cb) {
+	http.get(options, function (resp) {
 		var body = '';
 		resp.on('data', function (chunk) {
 			body += chunk;
 		}).on('end', function () {
 			var data = JSON.parse(body);
-			console.log('returning data');
 			cb(data);
 		}).on('error', function (e) {
 			console.log('error', e);
@@ -20,4 +17,4 @@
 };
 
 
-module.exports = getData;
+module.exports = new httpHelpers();
