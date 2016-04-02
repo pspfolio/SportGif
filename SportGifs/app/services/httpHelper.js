36c8a84d340c2c796b82537207ba6f79a31b4f79
PeppeS <pspfolio@gmail.com>
2016-04-02 19:40:06 +0300

    Adding statuscode (200) check to the httpget

--- SportGifs/app/services/httpHelper.js
+++ SportGifs/app/services/httpHelper.js
@@ -4,15 +4,17 @@ var httpHelpers = function () { };
 
 httpHelpers.prototype.getData = function (options, cb) {
 	http.get(options, function (resp) {
-		var body = '';
-		resp.on('data', function (chunk) {
-			body += chunk;
-		}).on('end', function () {
-			var data = JSON.parse(body);
-			cb(data);
-		}).on('error', function (e) {
-			console.log('error', e);
-		});
+		if (resp.statusCode === 200) {
+			var body = '';
+			resp.on('data', function (chunk) {
+				body += chunk;
+			}).on('end', function () {
+				var data = JSON.parse(body);
+				cb(data);
+			}).on('error', function (e) {
+				console.log('VIRHE', e);
+			});
+		}
 	});
 };
 
