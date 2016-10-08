df1393cd86524bdc8604b9d1d168f54a89dc85f7
Petteri Suominen <pspfolio@gmail.com>
2016-10-08 09:37:49 +0300

    getting data over https not from http

--- SportGifs/app/services/httpHelper.js
+++ SportGifs/app/services/httpHelper.js
@@ -1,14 +1,15 @@
-﻿var http = require('http');
+﻿var https = require('https');
 
 var httpHelpers = function () { };
 
 httpHelpers.prototype.getData = function (options, cb) {
-	http.get(options, function (resp) {
+	https.get(options, function (resp) {
 		if (resp.statusCode === 200) {
 			var body = '';
 			resp.on('data', function (chunk) {
 				body += chunk;
 			}).on('end', function () {
+				console.log('resp statusCode 200');
 				var data = JSON.parse(body);
 				cb(data);
 			}).on('error', function (e) {
